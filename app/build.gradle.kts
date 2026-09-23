73896a5a6eb980118ad3541f430f8be5338424cc
Clarco Mada Digital <it-ia@mada-digital.net>
2026-09-23 07:39:57 +0300

    Version 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "net.mada.lumea"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.1.0"
         vectorDrawables { useSupportLibrary = true }
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
